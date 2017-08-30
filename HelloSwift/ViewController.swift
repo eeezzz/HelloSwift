a0bc5e4e6d51b34ac5de78eb8a146a2d7bc5c5bd
giming <giming@hsin-yi.org.tw>
2017-08-30 17:37:54 +0800

    change to iphone6

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        labelTitle.text = "老公你被盜號～"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
